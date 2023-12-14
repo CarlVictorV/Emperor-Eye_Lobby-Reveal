368313e6d6eb2918f623d3be616391120eb80c9e
CarlVictorV <19104214@usc.edu.ph>
2023-12-15 05:03:19 +0800

    progress

--- core/testing.py
+++ core/testing.py
@@ -120,47 +120,50 @@ async def connect(connection):
         'Authorization': 'Basic ' + riotclient_session_token
     }
 
-    check = '/player-account/aliases/v1/display-name'
-    check = riotclient_api + check
-    r = requests.get(check, headers=riotclient_headers, verify=False)
+    get_lobby = riotclient_api + '/chat/v5/participants'
+    r = requests.get(
+        get_lobby, headers=riotclient_headers, verify=False)
     r = json.loads(r.text)
-    
-    if r == '':
-        print("You are not logged in")
-        exit(0)
-    else:
-        current_gamename = r['gameName']
-        current_tagline = r['tagLine']
-        current_summoner = current_gamename + "#" + current_tagline
-        print("You are logged in as " + current_summoner)
+    print(r)
 
-    
-    try:
-        account_dto = account._account.by_riot_id(my_area, current_gamename, current_tagline)
-    except ApiError:
-        print("your api key is not valid")
-        exit(0)
-    print("your api key is valid")
-    print(account_dto)
-    get_summoner_details = watcher.summoner.by_puuid(my_region, account_dto['puuid'])
-    print(get_summoner_details)
-    get_ranked_stats = watcher.league.by_summoner(my_region, get_summoner_details['id'])
-    print(get_ranked_stats)
+    # check = '/player-account/aliases/v1/display-name'
+    # check = riotclient_api + check
+    # r = requests.get(check, headers=riotclient_headers, verify=False)
+    # r = json.loads(r.text)
 
-    for j in get_ranked_stats:
-        if j['queueType'] == 'RANKED_SOLO_5x5':
-            print(j['tier'] + " " + j['rank'])
-            print(j['leaguePoints'])
-            print(j['wins'])
-            print(j['losses'])
-            print(j['veteran'])
-            print(j['inactive'])
-            print(j['freshBlood'])
-            print(j['hotStreak'])
+    # if r == '':
+    #     print("You are not logged in")
+    #     exit(0)
+    # else:
+    #     current_gamename = r['gameName']
+    #     current_tagline = r['tagLine']
+    #     current_summoner = current_gamename + "#" + current_tagline
+    #     print("You are logged in as " + current_summoner)
 
-    # ranked_stats = watcher.league.by_id
+    # try:
+    #     account_dto = account._account.by_riot_id(my_area, current_gamename, current_tagline)
+    # except ApiError:
+    #     print("your api key is not valid")
+    #     exit(0)
+    # print("your api key is valid")
+    # print(account_dto)
+    # get_summoner_details = watcher.summoner.by_puuid(my_region, account_dto['puuid'])
+    # print(get_summoner_details)
+    # get_ranked_stats = watcher.league.by_summoner(my_region, get_summoner_details['id'])
+    # print(get_ranked_stats)
 
+    # for j in get_ranked_stats:
+    #     if j['queueType'] == 'RANKED_SOLO_5x5':
+    #         print(j['tier'] + " " + j['rank'])
+    #         print(j['leaguePoints'])
+    #         print(j['wins'])
+    #         print(j['losses'])
+    #         print(j['veteran'])
+    #         print(j['inactive'])
+    #         print(j['freshBlood'])
+    #         print(j['hotStreak'])
 
+    # # ranked_stats = watcher.league.by_id
 
 
 connector.start()
