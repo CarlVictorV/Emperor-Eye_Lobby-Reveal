4aebbdbae3fd324072b70061752363e5a67c4c61
CarlVictorV <19104214@usc.edu.ph>
2023-12-15 02:43:23 +0800

    Update linkmaker.py
    Deeplol.gg seems to be bugged.
    Specifically the website PH version

--- core/linkmaker.py
+++ core/linkmaker.py
@@ -4,6 +4,13 @@ import webbrowser
 class multi_search:
     def __init__(self, nameArr):
         self.nameArr = nameArr
+        self.opgg()
+        self.ugg()
+        # self.deeplolgg()
+
+
+    def open_link(self, link):
+        webbrowser.open(link)
 
     # Websites for multi search
         # op.gg
@@ -27,9 +34,35 @@ class multi_search:
         # '#' = %23
 
    # TODO: Implement the multi search
+    
+    def opgg(self):
+        link = 'https://www.op.gg/multisearch/ph?summoners='
+        for i in self.nameArr:
+            link += i.replace(' ', '+').replace(',', '%2C').replace('#', '%23')
+            link += '%2C'
+        self.opgg = link
+
+    def ugg(self):
+        link = 'https://u.gg/multisearch?summoners='
+        for i in self.nameArr:
+            link += i.replace(' ', '%20').replace('#', '-')
+            link += ','
+        link += '&region=ph2'
+        self.ugg = link
+
+    # PH seems to be broken
+    # def deeplolgg(self):
+    #     link = 'https://www.deeplol.gg/multi/PH/'
+    #     for i in self.nameArr:
+    #         link += i.replace(' ', '%20').replace(',', '%2C').replace('#', '%23')
+    #         link += '%2C'
+    #     self.deeplolgg = link
+
+
 
 
 if __name__ == '__main__':
     nameArr = ['KaiserV#GOW', 'Mikado#khan',
-               'AustinReaves15#AR15', 'Aim and Miss#BOBO']
-    multi_search(nameArr)
+               'AustinReaves15#AR15', 'Aim and Miss#BOBO', 'Daryl #Ligo']
+    test = multi_search(nameArr)
+    print(test.deeplolgg)
